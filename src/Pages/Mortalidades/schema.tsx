be8feae5bcf3fb0f6bcb975ab43d463d43800e61
Anderson Brasil <andersonbrasil72@gmail.com>
2023-05-08 21:22:33 -0300

    Push080520232122

--- src/Pages/Mortalidades/schema.tsx
+++ src/Pages/Mortalidades/schema.tsx
@@ -5,7 +5,18 @@ export default Yup.object().shape({
     loteId: Yup.number().required("O Lote é um campo obrigatório"),
     aviarioId: Yup.number().required("O Aviário é um campo obrigatório"),
     causaMorte: Yup.number().required("A causa é um campo obrigatório"),
-    outraCausa: Yup.string().required("Descreva a outra causa da morte"),
+    // outraCausa: Yup.string().when("causaMorte", {
+    //     is: (causaMorte) => causaMorte !== 0,
+    //     then: Yup.string().required('"Descreva a outra causa da morte"')
+    // }),
+    outraCausa:  Yup.string().test(
+        'text_morte',
+        'Descreva a outra causa da morte',
+        function (value:any) {
+          const causaMorte = this.parent.causaMorte;
+          return value !== causaMorte;
+        }
+      ),
     box1Femea: Yup.number().typeError("Digite somente números").required("Box1 é um campo obrigatório"),
     box2Femea: Yup.number().typeError("Digite somente números").required("Box2 é um campo obrigatório"),
     box3Femea: Yup.number().typeError("Digite somente números").required("Box3 é um campo obrigatório"),
